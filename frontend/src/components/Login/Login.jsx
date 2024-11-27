ee753e41bb6d3751c415b166f0a27bf43d3a0181
JuanLariasMendez <jlariasm@miumg.edu.gt>
2024-11-27 03:23:54 -0600

    feat: Recibimiento de credenciales en frontend Login
    Hasta el momento ya se reciben las credenciales colocadas en el formulario del login

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,14 +1,48 @@
 import "./Login.css";
+import { useState } from "react";
 
 const Login = () => {
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  /**
+   * Maneja el evento de inicio de sesión.
+   *
+   * @param {Event} e - El evento de envío del formulario.
+   * @returns {void}
+   */
+  const handdleLogin = (e) => {
+    e.preventDefault();
+    // const username = e.target[0].value;
+    // const password = e.target[1].value;
+    console.log({
+      username: username,
+      password: password,
+    });
+  };
+
   return (
     <div>
       <form action="">
         <label className="custom-label">Username:</label>
-        <input type="text" className="custom-input" />
+        <input
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+          placeholder="username"
+          className="custom-input"
+          type="text"
+        />
         <label className="custom-label">Password:</label>
-        <input type="password" className="custom-input" />
-        <button type="submit" className="custom-button">
+        <input
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+          placeholder="password"
+          className="custom-input"
+          type="password"
+        />
+        <button type="submit" className="custom-button" onClick={handdleLogin}>
           Login
         </button>
       </form>
